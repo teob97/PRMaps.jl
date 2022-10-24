1c61735e948c4f711bb15805faaf58966a2d9c81
Baratto Matteo <teo.baratto.97@gmail.com>
2022-10-24 15:39:32 +0200

    Generated first map

--- src/PrmMaps.jl
+++ src/PrmMaps.jl
@@ -1,5 +1,38 @@
 module PrmMaps
 
-greet() = print("Hello World!")
+import Stripeline as Sl
+using Healpix
+export makeMap
+
+function makeMap(cam_ang :: Sl.CameraAngles, 
+                 telescope_ang :: Sl.TelescopeAngles, 
+                 times, τ_s, signal, NSIDE)
+    
+    pixel_index = Array{Int}(undef, length(times))
+
+    (dirs, Ψ) = Sl.genpointings(cam_ang, times; telescope_ang = telescope_ang) do time_s
+        return (0.0, deg2rad(20.0), Sl.timetorotang(time_s, τ_s*60.))
+    end
+    
+    for i in 1:length(times)
+        colat, long = dirs[i,:]
+        pixel_index[i] = ang2pix(signal, colat, long)
+    end
+    
+    sky_tod = signal.pixels[pixel_index]
+    map_values = Sl.tod2map_mpi(pixel_index, sky_tod, 12*(NSIDE^2); unseen = UNSEEN)
+
+    map = HealpixMap{Float64, RingOrder}(NSIDE)
+    map.pixels = map_values
+    
+    map
+end
+
+# Importare .fits
+# Generare puntamenti
+# cilare sui puntamenti e trovare indici associati ai pixel che vedo
+# genero TOD
+# tod2map
+# plottare mappa
 
 end # module PrmMaps
